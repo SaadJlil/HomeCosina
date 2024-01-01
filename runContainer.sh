2dbb900f303509035171300a0080d86ff5750f10
SaadJlil <sailjlad@gmail.com>
2024-01-01 15:36:11 -0800

    pre reverse prx implementation: Making sure docker only responds to localhost

--- runContainer.sh
+++ runContainer.sh
@@ -6,11 +6,24 @@ AUTH_PORT=$(grep '^PORT=' ./AuthIdt/.env | cut -d '=' -f 2)
 # Extract port from Mvp/.env
 MVP_PORT=$(grep '^PORT=' ./Mvp/.env | cut -d '=' -f 2)
 
+#Only responds to localhost connections (Cons: specification of host ports)
+sudo docker system prune -a &&
+sudo docker build \
+  -t homecosina . &&
+sudo docker run \
+  -p 127.0.0.1:3000:$AUTH_PORT \
+  -p 127.0.0.1:5000:$MVP_PORT \
+  -it homecosina #-s
 
+exit
+
+
+
+#Responds to all connection (Not restricted to localhost)
 sudo docker system prune -a &&
 sudo docker build \
   --build-arg AUTH_PORT_ARG=$AUTH_PORT \
   --build-arg MVP_PORT_ARG=$MVP_PORT \
   -t homecosina . &&
-sudo docker run -itP homecosina -s &
+sudo docker run -itP homecosina #-s
 
